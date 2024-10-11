96bd85e2c9b7f95c68597dc46f3873f961f2010d
yillkid <yillkid@gmail.com>
2024-10-12 00:23:12 +0800

    train_test_split_iris

--- train_test_split.py
+++ train_test_split.py
@@ -1,8 +1,9 @@
+from sklearn import datasets
 from sklearn.model_selection import train_test_split
 
-data = [n for n in range(1, 11)]
-print(data)
+# 將 Iris 資料集切割為訓練集和測試集
+iris = datasets.load_iris()
+X_train, X_test, y_train, y_test = train_test_split(iris["data"], iris["target"], test_size=0.3)
 
-train_data, test_data = train_test_split(data, random_state=777, train_size=0.8)
-print(train_data)
-print(test_data)
+print("X_train = " + str(X_train))
+print("X_test = " + str(X_test))
